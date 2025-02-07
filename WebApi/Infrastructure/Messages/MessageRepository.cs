303809bbc87c5a833f086ceafcf047fac582df37
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2025-02-07 21:11:05 +0400

    feat: add transaction on message save

--- WebApi/Infrastructure/Messages/MessageRepository.cs
+++ WebApi/Infrastructure/Messages/MessageRepository.cs
@@ -9,29 +9,42 @@ namespace Infrastructure.Messages;
 public class MessageRepository(IConfiguration configuration, MessageMapper mapper) : IMessageRepository
 {
 	private readonly string _connectionString = configuration.GetConnectionString("DefaultConnection") ??
-	                                           throw new ArgumentNullException(nameof(MessageRepository));
+	                                            throw new ArgumentNullException(nameof(MessageRepository));
 
-	public async Task SaveMessageAsync(Message message)
+	public async Task SaveAsync(Message message)
 	{
 		const string query =
 			"INSERT INTO Messages (Id, Text, Timestamp) VALUES (@Id, @Text, @Timestamp)";
 
 		await using var connection = new NpgsqlConnection(_connectionString);
 		await connection.OpenAsync();
-		await using var command = new NpgsqlCommand(query, connection)
+		await using var transaction = await connection.BeginTransactionAsync();
+
+		try
 		{
-			CommandType = CommandType.Text
-		};
+			await using var command = new NpgsqlCommand(query, connection, transaction)
+			{
+				CommandType = CommandType.Text
+			};
 
-		var messageEntity = mapper.ToMessageEntity(message);
-		command.Parameters.AddWithValue("Id", messageEntity.Id);
-		command.Parameters.AddWithValue("Text", messageEntity.Text);
-		command.Parameters.AddWithValue("Timestamp", messageEntity.Timestamp);
+			var messageEntity = mapper.ToMessageEntity(message);
+			command.Parameters.AddWithValue("Id", messageEntity.Id);
+			command.Parameters.AddWithValue("Text", messageEntity.Text);
+			command.Parameters.AddWithValue("Timestamp", messageEntity.Timestamp);
 
-		await command.ExecuteNonQueryAsync();
+			await command.ExecuteNonQueryAsync();
+
+			await transaction.CommitAsync();
+		}
+		catch
+		{
+			await transaction.RollbackAsync();
+			throw;
+		}
 	}
 
-	public async Task<IEnumerable<Message>> GetMessagesByDateRangeAsync(DateTime from, DateTime to)
+
+	public async Task<IEnumerable<Message>> GetByDateRangeAsync(DateTime from, DateTime to)
 	{
 		const string query =
 			"SELECT Id, Text, Timestamp FROM Messages WHERE Timestamp BETWEEN @From AND @To";
@@ -60,30 +73,30 @@ public class MessageRepository(IConfiguration configuration, MessageMapper mappe
 		return mapper.ToMessage(entities);
 	}
 
-	public async Task<Message?> GetMessageByIdAsync(int messageId)
+	public async Task<Message?> GetByIdAsync(int messageId)
 	{
 		const string query = "SELECT Id, Text, Timestamp FROM Messages WHERE Id = @Id";
 		await using var connection = new NpgsqlConnection(_connectionString);
 		await connection.OpenAsync();
-		
+
 		await using var command = new NpgsqlCommand(query, connection)
 		{
 			CommandType = CommandType.Text
 		};
-		
+
 		command.Parameters.AddWithValue("Id", messageId);
 		await using var reader = await command.ExecuteReaderAsync();
 		if (!await reader.ReadAsync())
 		{
 			return null;
 		}
-		
+
 		var messageEntity = new MessageEntity(
 			reader.GetInt32(0),
 			reader.GetString(1),
 			reader.GetDateTime(2)
 		);
-		
+
 		return mapper.ToMessage(messageEntity);
 	}
 }
